57c43bc711f6b33dde03987018c9b3fcb2f2080e
lastis-citra <lastis@infoseek.jp>
2022-05-20 15:19:48 +0900

    FC2から情報を取るように

--- main.py
+++ main.py
@@ -60,10 +60,12 @@ def output_result(html):
     write_html(file1, html)
 
 
-def input_urls():
+def input_urls(debug):
     path = f'input_urls.txt'
     input_dict = {}
-    if os.path.exists(path):
+    if debug:
+        return {'FC2PPV_test': 'https://supjav.com/ja/?s=FC2PPV'}
+    elif os.path.exists(path):
         with open(path, 'r', errors='replace', encoding="utf_8") as file:
             line_list = file.read().splitlines()
             for line in line_list:
@@ -75,9 +77,9 @@ def input_urls():
         return {}
 
 
-def input_last_url(site_name):
+def input_last_url(site_name, debug):
     path = f'last_url_{site_name}.txt'
-    if os.path.exists(path):
+    if os.path.exists(path) and not debug:
         with open(path, 'r', errors='replace', encoding="utf_8") as file:
             line_list = file.read().splitlines()
         return line_list[0]
@@ -85,16 +87,18 @@ def input_last_url(site_name):
         return ''
 
 
-def output_first_url(url, site_name):
+def output_first_url(url, site_name, debug):
+    if debug:
+        return 0
     path = f'last_url_{site_name}.txt'
     with open(path, 'w', encoding='utf-8') as f:
         f.write(url)
     return 0
 
 
-def input_detail_ids():
+def input_detail_ids(debug):
     path = f'detail_ids.txt'
-    if os.path.exists(path):
+    if os.path.exists(path) and not debug:
         with open(path, 'r', errors='replace', encoding="utf_8") as file:
             line_list = file.read().splitlines()
         # print(f"detail_ids: {','.join(line_list)}")
@@ -103,7 +107,9 @@ def input_detail_ids():
         return []
 
 
-def output_detail_ids(detail_ids):
+def output_detail_ids(detail_ids, debug):
+    if debug:
+        return 0
     path = f'detail_ids.txt'
     with open(path, 'w', encoding='utf-8') as f:
         f.write('\n'.join(detail_ids))
@@ -123,8 +129,33 @@ def get_next_page(soup):
         return None
 
 
+# FC2のHPから情報を取り出す
+def get_fc2_data(fc2_id):
+    url = f'https://adult.contents.fc2.com/article/{fc2_id}/'
+    # print(f'fc2_url: {url} ', end='')
+    scraper = cloudscraper.create_scraper(
+        browser={
+            'browser': 'chrome',
+            'platform': 'windows',
+            'desktop': True
+        }
+    )
+    res = scraper.get(url)
+    res.encoding = res.apparent_encoding
+    soup = BeautifulSoup(res.text, 'html.parser')
+    description = soup.select_one('meta[name="description"]')['content']
+    # 特に古いIDだと製品ページにたどり着けないため
+    if 'Unable' in description:
+        return ''
+    keywords = soup.select_one('meta[name="keywords"]')['content']
+    keywords = keywords.split(',Videos')[0]
+
+    # print(keywords)
+    return keywords
+
+
 # 検索結果ページを取得する
-def get_search_result(count, first_url, last_url, html, detail_ids, url):
+def get_search_result(count, first_url, last_url, html, detail_ids, url, debug):
     print(f'input_url: {url} ', end='')
     last_check = False
 
@@ -163,12 +194,24 @@ def get_search_result(count, first_url, last_url, html, detail_ids, url):
         img_src = post_tag.select_one('img')['data-original']
         img_tag = post_tag.select_one('img')
         img_tag.attrs['src'] = img_src
-        # aタグのテキストを短くする
         a_text = post_tag.select_one('div.con a').text
+        # FC2PPVのIDを抜き出す
+        fc2_ids = re.findall(r'FC2PPV\s(\d+)\s', a_text)
+        keywords = ''
+        user = ''
+        if len(fc2_ids) == 1:
+            keywords = get_fc2_data(fc2_ids[0])
+            if keywords != '':
+                user = ' (By ' + keywords.split(',')[0] + ') '
+        # aタグのテキストを短くする
         a_text = re.sub(r'FC2PPV\s\d+\s', '', a_text)
+        # userを付加する
+        a_text = user + a_text
         # [有]が含まれる場合は先頭に持ってくるように
         if '[有]' in a_text:
             a_text = '[有]' + a_text.replace('[有]', '')
+        if '無修正' in keywords:
+            a_text = '[無]' + a_text
         # print('a_text: ' + a_text)
         post_tag.select_one('div.con a').string.replace_with(a_text)
 
@@ -179,29 +222,33 @@ def get_search_result(count, first_url, last_url, html, detail_ids, url):
     next_url = get_next_page(soup)
 
     # 次のページが存在するなら，再帰的に実行
-    if next_url is not None and not last_check and count <= 2:
-        html, _, detail_ids = get_search_result(count + 1, first_url, last_url, html, detail_ids, next_url)
+    if next_url is not None and not last_check and not debug and count <= 1:
+        html, _, detail_ids = get_search_result(count + 1, first_url, last_url, html, detail_ids, next_url, debug)
 
     return html, first_url, detail_ids
 
 
-def main_process():
-    input_dict = input_urls()
+def main_process(debug):
+    input_dict = input_urls(debug)
     html = ''
-    detail_ids = input_detail_ids()
+    detail_ids = input_detail_ids(debug)
     first_url_dict = {}
 
     for site_name, site_url in input_dict.items():
-        last_url = input_last_url(site_name)
-        html, first_url, detail_ids = get_search_result(0, '', last_url, html, detail_ids, site_url)
+        last_url = input_last_url(site_name, debug)
+        html, first_url, detail_ids = get_search_result(0, '', last_url, html, detail_ids, site_url, debug)
         first_url_dict[site_name] = first_url
 
     output_result(html)
-    output_detail_ids(detail_ids)
+    output_detail_ids(detail_ids, debug)
 
     for site_name, first_url in first_url_dict.items():
-        output_first_url(first_url, site_name)
+        output_first_url(first_url, site_name, debug)
 
 
 if __name__ == '__main__':
-    main_process()
+    debug_string = os.environ.get('DEBUG')
+    debug_bool = False
+    if debug_string == 'True':
+        debug_bool = True
+    main_process(debug_bool)
